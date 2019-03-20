246d9dc58377ac45ecbf83092efdbf7b9ffd3660
Vandesm14 <vandesm14@hyperjs-servers.com>
2019-03-19 20:44:44 -0400

    Backup Files

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var x = $('.tab_title[data-tabfirst]');
+$('.tab', x).toggleClass('active', true);
+$('.tab_box .tab_content[data-tabid="' + $(x).data('tabidp') + '"]').show();
+
 $('.tab').on('click', function(){
 	$('.tab_box .tab_content').hide();
 	$('.tab').toggleClass('active', false);
