a40316a7308ca204c85b0c6dc10679bdf5bb4b68
Vandesm14 <vandesm14@hyperjs-servers.com>
2019-04-15 21:03:31 -0400

    Backup Files

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ var facilityTemplate = {
 };
 
 var serverTemplate = {
+	status: 1,
 	hdd: 0,
 	cpu: 0
 };
@@ -38,7 +39,10 @@ var stats = {};
 var gameVars = { // Holding Variables for calculations
 	expenses: {
 		pcu: 0 // Total PCU
-	}
+	},
+	visitors: 0,
+	pages: 0,
+	ads: 0
 };
 
 // var facilities = [{
@@ -60,26 +64,65 @@ var expenses = { // Conversions for $$ per day
 	property: 333 * facilities.length,
 	electricity: gameVars.expenses.pcu * 0.114,
 	network: 0
-}
+};
 
 /* --------------------------- JQuery Liteners --------------------------- */
-var x = $('.tab_title[data-tabfirst]');
-$('.tab', x).toggleClass('active', true);
-$('.tab_box .tab_content[data-tabid="' + $(x).data('tabidp') + '"]').show();
+/* -------- Custom Tabs -------- */
+$(document).ready(function () {
+	var x = $('.tab_title[data-tabfirst]');
+	$('.tab', x).toggleClass('active', true);
+	$('.tab_box .tab_content[data-tabid="' + $(x).data('tabidp') + '"]').show();
 
-$('.tab').on('click', function () {
-	$('.tab_box .tab_content').hide();
-	$('.tab').toggleClass('active', false);
-	$(this).toggleClass('active', true);
-	$('.tab_box .tab_content[data-tabid="' + $('.tab_title', this).data('tabidp') + '"]').show();
+	$('.tab').on('click', function () {
+		$('.tab_box .tab_content').hide();
+		$('.tab').toggleClass('active', false);
+		$(this).toggleClass('active', true);
+		$('.tab_box .tab_content[data-tabid="' + $('.tab_title', this).data('tabidp') + '"]').show();
+	});
+
+	/* -------- Custom Store -------- */
+	$('.item_row_content').on('click', function () {
+		if (!$(this).hasClass('disabled')) {
+			var fIndex = $('#facilitySelector').val(); // Facility Index
+			var sIndex = $('#serverSelector').val(); // Server Index
+			console.log('Tab: ' + $(this).closest('.tab_content').data('tabname'));
+			console.log('Row: ' + $(this).closest('.item_row').data('rowname'));
+			console.log('Level: ' + ($(this).index() + 1));
+			switch($(this).closest('.tab_content').data('tabname')) {
+				case 'Hardware':
+					switch ($(this).closest('.item_row').data('rowname')) {
+						case 'HDD':
+							if (facilities[fIndex].servers[sIndex].hdd === undefined) {
+								facilities[fIndex].servers[sIndex].hdd = $(this).index();
+							} else {
+								if (confirm(`Are you sure you want to replace the ${$(this).closest('.item_row').data('rowname')} of Server ${} from Facility ${}?`))
+							}
+							break;
+						case 'SSD':
+							
+							break;
+						case 'CPU':
+							
+							break;
+					}
+					break;
+				case 'Servers':
+					
+					break;
+				case 'Facilities':
+					
+					break;
+				case 'Ads':
+
+					break;
+			}
+		}
+	});
 });
 
 /* --------------------------- Sequences --------------------------- */
 function startSequence() {
-	newFacility();
-	newServer(0);
-	// newComponent();
-	// newPage();
+	// alert('You have currently have no servers. To get one, pick a motherboard below');
 }
 
 function mainSequence() {
@@ -94,6 +137,14 @@ function updateServerList() {
 
 }
 
+function updateStorePage() {
+
+}
+
+function updateStats() {
+
+}
+
 /* --------------------------- Computing Functions --------------------------- */
 function computePower() {
 	// Run though all facilities + servers and gather total power expense
@@ -101,7 +152,15 @@ function computePower() {
 	// post the power to website
 }
 
-/* --------------------------- Helper Functions --------------------------- */
+function computeNetwork() {
+
+}
+
+function computeVisitors() {
+
+}
+
+/* --------------------------- Game Functions --------------------------- */
 function newFacility() {
 	facilities.push(cloneObj(facilityTemplate));
 }
